ba7295b8a3f5ce034b5238ffeee10d4038698d10
usuihiroya <usuihiroya.tech2@gmail.com>
2025-07-26 17:33:11 +0900

    AWS credential, terraform init 処理追加 リトライ

--- 10_infra/main.tf
+++ 10_infra/main.tf
@@ -10,8 +10,8 @@ terraform {
     }
   }
   backend "s3" {
-    bucket  = "{YOUR-S3-BUCKET-NAME}"
-    key     = "{YOUR-S3-BUCKET-KEY}"
+    bucket  = "{tastylog-bucket-usuihiroya }"
+    key     = "{terraform.tfstate}"
     region  = "ap-northeast-1"
     profile = "terraform"
   }
